2a2d1ac22fd779799892c74b4bf092531239017d
YanaCode <yanas6576@gmail.com>
2022-11-12 08:49:11 -0500

    updated current temperature

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
   temperatureElement.innerHTML = Math.round (response.data.main.temp);
   cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
+
 
 let apiKey = "71ebeeaed9cc1b5740f1ef2da025fc0a";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
