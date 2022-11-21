2d3f554d45ed26526492604b76d02fd4fc793042
YanaCode <yanas6576@gmail.com>
2022-11-21 15:53:37 -0500

    updated API url

--- src/app.js
+++ src/app.js
@@ -53,7 +53,7 @@ forecastElement.innerHTML = forecastHTML;
 function getForecast(coordinates) {
 console.log(coordinates);
 let apiKey = "96ad27349a64ea1dcdfbe6f4d458c085";
-let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayForecast);
 } 
 
