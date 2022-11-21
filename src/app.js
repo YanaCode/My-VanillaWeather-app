e92de475d9fdd126fd0a48ea8a2e3a5f23fc7a36
YanaCode <yanas6576@gmail.com>
2022-11-21 16:50:35 -0500

    updated function day

--- src/app.js
+++ src/app.js
@@ -28,19 +28,18 @@ let forecastElement = document.querySelector("#forecast");
 let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 let forecastHTML = `<div class="row">`;
-days.forEach(function(day){
+days.forEach(function (day) {
 forecastHTML = forecastHTML + `
 
               <div class="col-2">
                 <div class="weather-forecast-date">
-                ${day}
-              </div>
+                ${day}</div>
                 <img src="http://openweathermap.org/img/wn/04n@2x.png" alt="" width="35"/>
-                <div class="weather-forecast-temp">
-                 <span class="weather-forecast-temp-max">
-                  15° /</span>
-                  <span class="weather-temp-forecast-min">
-                   10° 
+                <div class="weather-forecast-temperatures">
+                 <span class="weather-forecast-temperature-max">
+                  ° /</span>
+                  <span class="weather-forecast-temperature-min">
+                   ° 
                   </span>
                   </div>
               </div>
@@ -51,7 +50,7 @@ forecastElement.innerHTML = forecastHTML;
 }
 
 function getForecast(coordinates) {
-console.log(coordinates);
+
 let apiKey = "96ad27349a64ea1dcdfbe6f4d458c085";
 let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayForecast);
@@ -76,11 +75,9 @@ function displayTemperature(response) {
  dateElement.innerHTML = formatDate(response.data.dt * 1000);
  iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 
- getForecast(response.data.coord);
+ getForecast(response.data.coordinates);
 
 }
-
-
 
 function search(city) {
   let apiKey = "71ebeeaed9cc1b5740f1ef2da025fc0a";
