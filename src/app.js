9a432987c5b378dbbcb1e5652ece02e0710122f2
YanaCode <yanas6576@gmail.com>
2022-12-19 15:41:49 -0500

    new updates

--- src/app.js
+++ src/app.js
@@ -36,24 +36,20 @@ function formateDay(timestamp){
   return days[day];
 }
 function displayForecast(response) {
-  console.log(response.data.daily);
+let forecast = response.data.daily;
 let forecastElement = document.querySelector("#forecast");
-
-let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
 let forecastHTML = `<div class="row">`;
 days.forEach(function (forecastDay, index) { 
-  if (index<5) {
+  if (index < 6) {
 forecastHTML = forecastHTML + `
 
               <div class="col-2">
-                <div class="weather-forecast-date">
-                ${formateDay(forecastDay.dt)}</div>
-                <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}04n@2x.png" alt="" width="35"/>
+                
+                <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="35"/>
                 <div class="weather-forecast-temperatures">
-                 <span class="weather-forecast-temperature-max">
+                 <span class="weather-forecast-temperature-max"> ${Math.round(forecastDay.temp.max)}
                   ° /</span>
-                  <span class="weather-forecast-temperature-min">
+                  <span class="weather-forecast-temperature-min"> ${Math.round(forecastDay.temp.min)}
                    ° 
                   </span>
                   </div>
@@ -91,7 +87,7 @@ function displayTemperature(response) {
  dateElement.innerHTML = formatDate(response.data.dt * 1000);
  iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 
- getForecast(response.data.coordinates);
+ getForecast(response.data.coord);
 
 }
 
